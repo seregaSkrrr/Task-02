1e5eebd9eeb17e8114b0ad14e90a0848b94db40a
seregaSkrrr <kravtsovserk@gmail.com>
2023-10-23 16:46:10 +0300

    ara

---
+++ DZ1/DZ1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <random>
+#include "Functions.h"
+#include "Matrix.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+    srand(time(NULL));
+
+    const int Size = 20;
+    int Array[Size];
+    int _number;
+    int number;
+
+    //cout << "Массив: ";
+    //Initial(Array, Size);
+    //Print(Array, Size);
+
+    //MaxAndMin(Array, Size);
+
+    //cout << "Отсортированный массив: ";
+    //Sort(Array, Size);    
+    //Print(Array, Size);
+    //cout << "Введите число для поиска: ";
+    //cin >> _number;
+    //BinarySearch(_number, Array, Size);
+
+    //cout << "Выберите номер какого элемента заменить: ";
+    //cin >> _number;
+    //cout << "Какое число поставить заместо: ";
+    //cin >> number;
+    //SwitchElement(Array, Size, _number, number);
+    //cout << "Исправленный массив: ";
+    //Print(Array, Size);
+
+    
+
+}
---
+++ DZ1/Functions.h
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+template<typename T>
+void Initial(T Array, size_t Array_Size) {
+
+    for (int Temp = 0; Temp < Array_Size; Temp++) {
+        Array[Temp] = rand() % 100 + 1;
+    }
+}
+
+template<typename T>
+void Print(T Array, size_t Array_Size) {
+
+    for (int Temp = 0; Temp < Array_Size; Temp++) {
+        cout << "[" << Array[Temp] << "] ";
+    }
+    cout << endl;
+    cout << endl;
+}
+
+template<typename T>
+void MaxAndMin(T Array, size_t Array_Size) {
+
+    int Minimal = Array[0], Maximum = Array[Array_Size - 1];
+    for (int Temp = 0; Temp < Array_Size; Temp++) {
+        if (Minimal > Array[Temp])
+            Minimal = Array[Temp];
+        else if (Maximum < Array[Temp])
+            Maximum = Array[Temp];
+    }
+    cout << "Максимальный элемент: " << Maximum << endl;
+    cout << "Минимальный элемент: " << Minimal << endl;
+    cout << endl;
+}
+
+template<typename T>
+void Sort(T Array, size_t Array_Size) {
+    for (int Temp1 = Array_Size - 1; Temp1 > 0; Temp1--) {
+        for (int Temp = 0; Temp < Temp1; Temp++) {
+            if (Array[Temp] > Array[Temp + 1]) {
+                swap(Array[Temp], Array[Temp + 1]);
+            }
+        }
+    }
+}
+
+template<typename T>
+void BinarySearch(int Number, T Array, size_t Array_Size) {
+    int Middle = 0;
+    int Right = Array_Size;
+    int Left = 0;
+    while (1){
+        Middle = (Left + Right) *0.5;
+
+        if (Number < Array[Middle])
+            Right = Middle - 1;
+        else if (Number > Array[Middle])
+            Left = Middle + 1;
+        else {
+            cout << "Ваше число на позиции: " << Middle+1;
+            cout << endl << endl;
+            break;
+        }
+
+        if (Left > Right) {
+            cout << "Такого числа нет в данном массиве" << endl << endl;
+            break;
+        }
+    }
+}
+
+template<typename T>
+void SwitchElement(T Array, size_t Array_Size, int Element, int Number) {
+    Array[Element-1] = Number;
+}
